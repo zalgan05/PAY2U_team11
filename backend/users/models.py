00f32abc0a78e3c39c68d8da622608c268dc460c
Артём Мариненко <zalgan@mail.ru>
2024-03-22 16:29:47 +0300

    Модель юзера

---
+++ backend/users/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+MAX_LENGTH = 64
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+
+    first_name = models.CharField(
+        max_length=MAX_LENGTH,
+        verbose_name='Имя'
+    )
+    middle_name = models.CharField(
+        max_length=MAX_LENGTH,
+        verbose_name='Отчество'
+    )
+    last_name = models.CharField(
+        max_length=MAX_LENGTH,
+        verbose_name='Фамилия'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'{self.username}'
