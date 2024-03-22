9d9a5a9f9f75ce7acf5bbf7429582a07d6eed3f3
Артём Мариненко <zalgan@mail.ru>
2024-03-22 20:38:55 +0300

    Добавлен и подключен автосваггер

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularAPIView,
+    SpectacularRedocView
+)
 
 
 router = DefaultRouter()
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'docs/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='docs'
+    ),
+    path(
+        'redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc'
+    ),
 ]
