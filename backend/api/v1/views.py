c34556f0cff8db1fbccbe2372d6bf1329a9cd870
Артём Мариненко <zalgan@mail.ru>
2024-03-25 15:59:30 +0300

    API для получения списка подписок и деталей подписки

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from subscriptions.models import (
+    Subscription,
+)
+from .serializers import SubscriptionSerializer, SubscriptionDetailSerializer
+
+
+class SubscriptionViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return SubscriptionDetailSerializer
+        return super().get_serializer_class()
