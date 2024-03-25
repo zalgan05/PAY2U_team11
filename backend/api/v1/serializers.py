b9ce5bbfdcaf09d938b2ee5cfc44c527d991d312
Артём Мариненко <zalgan@mail.ru>
2024-03-25 16:33:24 +0300

    Добавлены методы добавления/удаления подписки в избранное

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
-from subscriptions.models import Subscription, Tariff
+from subscriptions.models import Subscription, Tariff, IsFavoriteSubscription
+
+User = get_user_model()
 
 
 class TariffSerializer(serializers.ModelSerializer):
@@ -32,3 +36,56 @@ class SubscriptionDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
         fields = ['id', 'name', 'description', 'type', 'cashback', 'tariffs']
+
+
+class IsFavoriteSerializer(serializers.ModelSerializer):
+    """Сериализатор для добавления и удаления подписки в избранное."""
+
+    class Meta:
+        model = Subscription
+        fields = ['id',]
+        read_only_fields = ['id',]
+
+    def validate(self, attrs):
+        """
+        Проверяет, добавлена ли подписка в избранное для текущего пользователя,
+        и удаляет ее в зависимости от запроса.
+        """
+        request = self.context.get('request')
+        user = request.user
+        sub_id = self.context.get('sub_id')
+        subscription = get_object_or_404(Subscription, id=sub_id)
+        if request.method == 'POST':
+            if IsFavoriteSubscription.objects.filter(
+                user=user,
+                subscription=subscription
+            ).exists():
+                raise serializers.ValidationError(
+                    'Вы уже добавили сервис в избранное'
+                )
+        elif request.method == 'DELETE':
+            if not IsFavoriteSubscription.objects.filter(
+                user=user,
+                subscription=subscription
+            ).exists():
+                raise serializers.ValidationError(
+                    'Вы не добавляли этот сервис в избранное'
+                )
+            else:
+                IsFavoriteSubscription.objects.filter(
+                    user=user, subscription=subscription
+                ).delete()
+                return super().validate(attrs)
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        """
+        Создает связь пользователя с подпиской и добавляет ее в избранное.
+        """
+        sub_id = self.context.get('sub_id')
+        subscription = get_object_or_404(Subscription, id=sub_id)
+        IsFavoriteSubscription.objects.create(
+            user=User,
+            subscription=subscription
+        )
+        return subscription
--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -1,9 +1,17 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+# from django.shortcuts import get_object_or_404
 
 from subscriptions.models import (
+    # IsFavoriteSubscription,
     Subscription,
 )
-from .serializers import SubscriptionSerializer, SubscriptionDetailSerializer
+from .serializers import (
+    SubscriptionSerializer,
+    SubscriptionDetailSerializer,
+    IsFavoriteSerializer
+)
 
 
 class SubscriptionViewSet(
@@ -18,3 +26,24 @@ class SubscriptionViewSet(
         if self.action == 'retrieve':
             return SubscriptionDetailSerializer
         return super().get_serializer_class()
+
+    @action(detail=True, methods=['post'])
+    def favorite(self, request, pk):
+        """Добавляет подписку в избранное для текущего пользователя."""
+        serializer = IsFavoriteSerializer(
+            data=request.data,
+            context={'request': request, 'sub_id': pk}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @favorite.mapping.delete
+    def delete_favorite(self, request, pk):
+        """Удаляет подписку из избранного для текущего пользователя."""
+        serializer = IsFavoriteSerializer(
+            data=request.data,
+            context={'request': request, 'recipe_id': pk}
+        )
+        serializer.is_valid(raise_exception=True)
+        return Response(status=status.HTTP_204_NO_CONTENT)
