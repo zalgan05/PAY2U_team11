2abc847bab32e0ec54ecf8a71bc60a4004fad2eb
Артём Мариненко <zalgan@mail.ru>
2024-04-04 22:29:29 +0300

    Запуск сервера в докер-контейнерах

---
+++ backend/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo "Running migrations..."
+python manage.py makemigrations;
+python manage.py migrate;
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput;
+
+# echo "Loading initial data..."
+# python manage.py loaddata user_dump.json;
+# python manage.py update_users;
+# python manage.py loaddata other_dump.json;
+
+echo "Starting Celery worker..."
+celery -A backend worker -l info --pool=solo --without-mingle --without-gossip &
+
+echo "Starting Celery beat..."
+celery -A backend beat --loglevel=info &
+
+echo "Copying static files..."
+cp -r /app/collected_static/. /backend_static/static/
+
+echo "Starting Gunicorn..."
+gunicorn --bind 0:8000 backend.wsgi;
